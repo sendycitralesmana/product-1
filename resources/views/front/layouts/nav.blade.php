385236938393274ddf09543fd5b69923ce3b0bc9
Lesmana <sendycitralesmana@gmail.com>
2024-02-05 09:50:37 +0700

    update

--- resources/views/front/layouts/nav.blade.php
+++ resources/views/front/layouts/nav.blade.php
@@ -11,9 +11,9 @@
 
                 <div class="right-top-bar flex-w h-full">
 
-                    <a href="/login" class="flex-c-m trans-04 p-lr-25">
+                    {{-- <a href="/login" class="flex-c-m trans-04 p-lr-25">
                         My Account
-                    </a>
+                    </a> --}}
 
                     <a href="/" class="flex-c-m trans-04 p-lr-25">
                         ID

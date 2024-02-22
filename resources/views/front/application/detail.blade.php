15b35bbc841624446a7f0fd3d813c8855971b9e9
Lesmana <sendycitralesmana@gmail.com>
2024-02-22 08:23:19 +0700

    update

--- resources/views/front/application/detail.blade.php
+++ resources/views/front/application/detail.blade.php
@@ -3,7 +3,7 @@
 @section('main')
 
 <!-- breadcrumb -->
-<div class="container p-t-10">
+<div class="container p-t-30">
     <div class="bread-crumb flex-w p-l-25 p-r-15 p-t-30 p-lr-0-lg">
         <a href="/" class="stext-109 cl8 hov-cl1 trans-04">
             Beranda

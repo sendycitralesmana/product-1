ed91a06c2fa0e33b9ae69e28bb8e42994f368e72
Lesmana <sendycitralesmana@gmail.com>
2024-02-23 13:24:25 +0700

    update

--- resources/views/front/banner/banner.blade.php
+++ resources/views/front/banner/banner.blade.php
@@ -3,7 +3,7 @@
     <div class="wrap-slick1 rs1-slick1">
         <div class="slick1">
             
-            <div class="item-slick1" style="background-image: url( {{ asset('images/banner.jpg') }} ); height: 500px">
+            <div class="item-slick1" style="background-image: url( {{ asset('images/banner.jpg') }} )">
                 <div class="container h-full">
                     <div class="flex-col-l-m h-full p-t-100 p-b-30">
                         

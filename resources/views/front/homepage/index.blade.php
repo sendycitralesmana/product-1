74be19c47d23d728fd6b6bb779269a37ca9ca7ad
Lesmana <sendycitralesmana@gmail.com>
2024-02-27 13:28:37 +0700

    update

--- resources/views/front/homepage/index.blade.php
+++ resources/views/front/homepage/index.blade.php
@@ -28,25 +28,25 @@
     @include('front.banner.banner')
 
     {{-- Category Products --}}
-    {{-- @include('front.product.product') --}}
+    @include('front.product.product')
 
     {{-- About --}}
     @include('front.about.about')
 
     {{-- Our Applications --}}
-    {{-- @include('front.application.application') --}}
+    @include('front.application.application')
 
     {{-- Our Gallery --}}
     @include('front.gallery.gallery')
     
     {{-- Our Blogs --}}
-    {{-- @include('front.blog.blog') --}}
+    @include('front.blog.blog')
 
     {{-- Contact us --}}
     @include('front.contact.contact')
     
     {{-- Our Clients --}}
-    {{-- @include('front.client.client') --}}
+    @include('front.client.client')
 
     {{-- Our Catalogue --}}
     @include('front.catalogue.catalogue')   
